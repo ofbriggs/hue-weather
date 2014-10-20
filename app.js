2981b6f210b2263df93998572933017bf43cf12f
Brendan <br.br.cat@gmail.com>
2014-10-19 22:43:21 -0400

    Added functional fns

--- app.js
+++ app.js
@@ -23,11 +23,66 @@ var secondsDiff = hourDiff * 3600;
 var currentUnixTime = ~~(currentDate.getTime() / 1000);
 var yesterUnixTime = ~~(yesterDate.getTime() / 1000);
 
+//takes an array of callbacks, gives fn the resolved values in the same order
+function all(callbackArr, fn){
+    var arr = [];
+    var callbackCount = callbackArr.length;
+    callbackArr.forEach(function(callback, idx){
+        callback.call(callback, function(value){
+            arr[idx] = value;
+            if (--callbackCount <= 0){
+                fn(arr);
+            }
+        });
+    });
+}
+
 function curry(fn){
     var args = [].slice.call(arguments, 1);
     return function(callback){
         fn.apply(fn, args.concat(callback));
     }
+}
+
+// x -> Cont x
+function pureCallback(a) {
+    return function (f) {return f(a);};
+}
+
+// Cont (a -> b) -> Cont a -> Cont b
+function ap(cont1, cont2){
+    var steve1, steve2, fn;
+    cont1(function(fun){
+        steve1 = fun;
+        if (fn !== undefined & steve2 !== undefined)
+            fn(steve1(steve2));
+    });
+    cont2(function(val){
+        steve2 = val;
+        if (fn !== undefined & steve1 !== undefined)
+            fn(steve1(steve2));
+    });
+    return function(fun){
+        if (steve1 !== undefined & steve2 !== undefined)
+            fun(steve1(steve2));
+        else
+            fn = fun;
+    }
+}
+
+// (a -> b) -> Cont a -> Cont b
+function cmap(fn, cont){
+    ap(pureCallback(fn), cont);
+}
+
+// (a -> b -> c) -> Cont a -> Cont b -> Cont c
+function liftA2(fn, conta, contb){
+    function badfn(a){
+        return function(b){
+            return fn(a,b);
+        }
+    }
+    ap(cmap(badfn, conta), contb);
 }
 
 function getForecast(time, fn){
@@ -44,20 +99,6 @@ function extractTemp(weatherObj){
     var max = today.apparentTemperatureMax;
 
     return (max + min) / 2;
-}
-
-//takes an array of callbacks, gives fn the resolved values in the same order
-function all(callbackArr, fn){
-    var arr = [];
-    var callbackCount = callbackArr.length;
-    callbackArr.forEach(function(callback, idx){
-        callback.call(callback, function(value){
-            arr[idx] = value;
-            if (--callbackCount <= 0){
-                fn(arr);
-            }
-        });
-    });
 }
 
 function forecastDiff(time1, time2, fn){
@@ -88,6 +129,23 @@ var displayResult = function(result) {
     console.log(JSON.stringify(result, null, 2));
 };
 
+function diffLightState(tempDiff){
+    var absoluteChange = Math.abs(tempDiff);
+    if (absoluteChange < 3){
+        state = lightState.create().on().rgb(0, 255, 0);
+    }
+    else if (tempDiff > 0){
+        state = lightState.create().on().rgb(255, 0, 0);
+    }
+    else if (tempDiff > 0){
+        state = lightState.create().on().rgb(0, 0, 255);
+    }
+    return state;
+}
+
+function offLightState(){
+    return lightState.create().transition(3600).off();
+}
 
 hue.locateBridges().then(function(bridge){
     return api = new HueApi(bridge[0].ipaddress, username);
@@ -96,17 +154,8 @@ hue.locateBridges().then(function(bridge){
 }).then(function(lights){
     lights.lights.forEach(function(light){
         forecastDiff(yesterUnixTime, currentUnixTime, function(tempDiff){
-            var state, state2 = lightState.create().transition(3600).off();
-            var absoluteChange = Math.abs(tempDiff);
-            if (absoluteChange < 3){
-                state = lightState.create().on().rgb(0, 255, 0);
-            }
-            else if (tempDiff > 0){
-                state = lightState.create().on().rgb(255, 0, 0);
-            }
-            else if (tempDiff > 0){
-                state = lightState.create().on().rgb(0, 0, 255);
-            }
+            var state = diffLightState(tempDiff);
+            var state2 = offLightState();
             api.setLightState(light.id, state);
             api.setLightState(light.id, state2);
        });
